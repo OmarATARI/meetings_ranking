63fb45177dff8f208b7ab71d31a42497634a4200
Omar ATARI <atari.omar@gmail.com>
2019-09-03 16:03:31 +0200

    added profile controller and template

---
+++ src/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserController extends AbstractController
+{
+    private $em;
+    private $userRepo;
+
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepository)
+    {
+        $this->em = $em;
+        $this->userRepo = $userRepository;
+    }
+
+    /**
+     * @Route("/profile", name="profile_user")
+     * @return Response
+     */
+    public function index(UserInterface $user)
+    {
+        $current_user = $this->userRepo->find($user->getId());
+
+        return $this->render('user/profile.html.twig', array(
+            'user' => $current_user
+        ));
+    }
+}
